85cd5032d4e8e4b1a823bbd286ff1d9eb02c7b96
Vasile Denisa-Georgiana <denisa.vasile@student.unitbv.ro>
2022-07-23 21:46:25 +0300

    Edit Hangman C#

--- Hangman2/Hangman2/obj/Debug/net6.0-windows/App.g.i.cs
+++ Hangman2/Hangman2/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Hangman2 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Hangman2 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public static void Main() {
             Hangman2.App app = new Hangman2.App();
             app.InitializeComponent();
